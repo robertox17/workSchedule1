b47ebfd2f492feab3534fb2c9eb3a57397b7d606
shaq <school@robertos-MBP.lan>
2022-08-09 16:16:56 -0700

    more code

--- script.js
+++ script.js
@@ -10,4 +10,32 @@ $('.saveBtn').on('click', function(event){
     var input= ($(this).siblings('textarea').val())
     localStorage.setItem(time,input)
 });
-
+$(document).ready (function () {
+    for (let i = 9; i < 18; i++){
+        $("#hour-"+ i + " .description").val(localStorage.getItem("hour-"+ i))
+      }
+      function checkTime(){
+        var currentHour= moment().hour()
+    console.log(currentHour)
+        $('.time-block').each(function(){
+        var blockHour= parseInt($(this).attr('id').split('-')[1])
+    console.log(blockHour)
+        if(blockHour < currentHour ){
+            $(this).removeClass('present')
+            $(this).removeClass('future')
+            $(this).addClass('past')
+        }else if(currentHour === blockHour) {
+            $(this).removeClass('future')
+            $(this).removeClass('past')
+            $(this).addClass('present')
+    
+        }else{
+            $(this).removeClass('present')
+            $(this).removeClass('past')
+            $(this).addClass('future')
+        }
+        })
+    }
+      checkTime()
+      setInterval(checkTime,2000)
+    });
